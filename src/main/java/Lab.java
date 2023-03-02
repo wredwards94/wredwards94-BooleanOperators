e9b20f15ef71f59ca4f11ae4769f7c344868bc58
Wesley Edwards <wredwards94@outlook.com>
2023-03-02 11:02:01 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,7 +14,7 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+        return (b == true) ? true: false;
     }
 
     /**
@@ -23,7 +23,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return (b == true) ? false: true;
     }
 
     /**
@@ -33,7 +33,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        return (b1 == true && b2 == true) ? true: false;
     }
 
     /**
@@ -43,7 +43,7 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        return (b1 == true || b2 == true) ? true: false;
     }
 
 
